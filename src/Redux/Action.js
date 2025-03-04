05e7a39bbe62a81d5b2aa512dd4b27d1efa162ca
rishikesh86 <rishikesh.kumar@omneelab.com>
2025-03-04 12:46:30 +0530

    change date for prod testing date-04-03-2025

--- src/Redux/Action.js
+++ src/Redux/Action.js
@@ -78,7 +78,7 @@ export const orderApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getAllOrder: builder.query({
       query: (payload = "default") => ({
-        url: `/api/v1/getAllOrder?page=${payload.page}&batchSize=10&order_status=${payload.activeButton}&searchTerm=${payload.searchInput}&start_date=2025-01-22&end_date=2025-02-28`,
+        url: `/api/v1/getAllOrder?page=${payload.page}&batchSize=10&order_status=${payload.activeButton}&searchTerm=${payload.searchInput}&start_date=2025-01-22&end_date=2025-03-04`,
         method: "GET",
       }),
       providesTags: ["order"],
